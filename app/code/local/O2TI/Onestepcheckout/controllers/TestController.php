e4e5a1a64328c5d258f1b972d896ca39ca3a2c93
Italo A. Gomes <italo@amorimgomes.com.br>
2014-08-04 08:01:56 -0300

    removed one step checkout

--- app/code/local/O2TI/Onestepcheckout/controllers/TestController.php
+++
@@ -1,8 +0,0 @@
-<?php
-class O2TI_Onestepcheckout_TestController extends Mage_Core_Controller_Front_Action
-{
-		public function indexAction()
-		{
-			# echo Mage::getUrl('onestepcheckout/index/updatebillingform');
-		}
-}
